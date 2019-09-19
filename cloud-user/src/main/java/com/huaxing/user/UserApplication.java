94e0faae9043941c653fb39355f341fc0a2715d9
zion <for_ly@foxmail.com>
2019-09-19 17:31:48 +0800

    feat 增加网关工程

--- cloud-user/src/main/java/com/huaxing/user/UserApplication.java
+++ cloud-user/src/main/java/com/huaxing/user/UserApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 
 @Slf4j
 @MapperScan("com.huaxing.user.mapper")
@@ -17,6 +18,4 @@ public class UserApplication{
     public static void main(String[] args) {
         SpringApplication.run(UserApplication.class, args);
     }
-
-
 }
